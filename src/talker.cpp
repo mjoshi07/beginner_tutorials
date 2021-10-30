5f3531f3004c607ea8f643c8a84fec0bfc0819e4
mjoshi07 <mayankjoshi63gmail.com>
2021-10-30 13:08:10 -0400

    rostopic chatter_bot added, upaded CMakelists

--- src/talker.cpp
+++ src/talker.cpp
@@ -1,9 +1,10 @@
 #include "ros/ros.h"
+#include "std_msgs/String.h"
 
 #include <sstream>
 
 /**
- * This tutorial initializes a "talker node" which will be used as a publisher
+ * This tutorial initializes a "talker node" which publishes on topic "chatter_bot"
  */
 int main(int argc, char **argv)
 {
@@ -18,6 +19,32 @@ int main(int argc, char **argv)
    * part of the ROS system.
    */
   ros::init(argc, argv, "talker_node");
+  /**
+   * NodeHandle is the main access point to communications with the ROS system.
+   * The first NodeHandle constructed will fully initialize this node, and the last
+   * NodeHandle destructed will close down the node.
+   */
+  ros::NodeHandle n;
+  
+  /**
+ * The advertise() function is how you tell ROS that you want to
+ * publish on a given topic name. This invokes a call to the ROS
+ * master node, which keeps a registry of who is publishing and who
+ * is subscribing. After this advertise() call is made, the master
+ * node will notify anyone who is trying to subscribe to this topic name,
+ * and they will in turn negotiate a peer-to-peer connection with this
+ * node.  advertise() returns a Publisher object which allows you to
+ * publish messages on that topic through a call to publish().  Once
+ * all copies of the returned Publisher object are destroyed, the topic
+ * will be automatically unadvertised.
+ *
+ * The second parameter to advertise() is the size of the message queue
+ * used for publishing messages.  If messages are published more quickly
+ * than we can send them, the number here specifies how many messages to
+ * buffer up before throwing some away.
+ */
+
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter_bot", 1000);
 
   return 0;
 }
