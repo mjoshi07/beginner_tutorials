8935b217c3c088b161ee669b107c507caec091dd
mjoshi07 <mayankjoshi63gmail.com>
2021-10-30 12:55:24 -0400

    Added talker.cpp which initializes talker node

---
+++ src/talker.cpp
@@ -0,0 +1,23 @@
+#include "ros/ros.h"
+
+#include <sstream>
+
+/**
+ * This tutorial initializes a "talker node" which will be used as a publisher
+ */
+int main(int argc, char **argv)
+{
+  /**
+   * The ros::init() function needs to see argc and argv so that it can perform
+   * any ROS arguments and name remapping that were provided at the command line.
+   * For programmatic remappings you can use a different version of init() which takes
+   * remappings directly, but for most command-line programs, passing argc and argv is
+   * the easiest way to do it.  The third argument to init() is the name of the node.
+   *
+   * You must call one of the versions of ros::init() before using any other
+   * part of the ROS system.
+   */
+  ros::init(argc, argv, "talker_node");
+
+  return 0;
+}
