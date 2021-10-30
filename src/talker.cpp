8abd61869c63119324a136a54a03e8a460df3aa9
mjoshi07 <mayankjoshi63gmail.com>
2021-10-30 13:17:24 -0400

    Added chatter_bot publisher and subscriber, updated CMakeLists

--- src/talker.cpp
+++ src/talker.cpp
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
    * NodeHandle destructed will close down the node.
    */
   ros::NodeHandle n;
-  
+
   /**
  * The advertise() function is how you tell ROS that you want to
  * publish on a given topic name. This invokes a call to the ROS
@@ -45,6 +45,41 @@ int main(int argc, char **argv)
  */
 
   ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter_bot", 1000);
+
+  ros::Rate loop_rate(10);
+
+  /**
+   * A count of how many messages we have sent. This is used to create
+   * a unique string for each message.
+   */
+  int count = 0;
+  while (ros::ok())
+  {
+    /**
+     * This is a message object. You stuff it with data, and then publish it.
+     */
+    std_msgs::String msg;
+
+    std::stringstream ss;
+    ss << "hello world " << count;
+    msg.data = ss.str();
+
+    ROS_INFO("%s", msg.data.c_str());
+
+    /**
+     * The publish() function is how you send messages. The parameter
+     * is the message object. The type of this object must agree with the type
+     * given as a template parameter to the advertise<>() call, as was done
+     * in the constructor above.
+     */
+    chatter_pub.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }
+
 
   return 0;
 }
